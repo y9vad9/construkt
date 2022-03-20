a61de048151fe652777d21cf843aedf01cad3417
vadim <y9vad9@gmail.com>
2022-03-20 22:19:21 +0200

    feat: added `marginLayoutParams()`

--- core/src/main/java/com/construkt/LayoutParams.kt
+++ core/src/main/java/com/construkt/LayoutParams.kt
@@ -1,40 +1,62 @@
 package com.construkt
 
 import android.view.ViewGroup.LayoutParams
+import android.view.ViewGroup.MarginLayoutParams
 
 public fun layoutParams(): LayoutParams = LayoutParams(LayoutParams.WRAP_CONTENT, LayoutParams.WRAP_CONTENT)
+public fun marginLayoutParams(): MarginLayoutParams = MarginLayoutParams(LayoutParams.WRAP_CONTENT, LayoutParams.WRAP_CONTENT)
 
-public fun LayoutParams.maxSize(): LayoutParams = apply {
+public fun <T : LayoutParams> T.maxSize(): T = apply {
     width = LayoutParams.MATCH_PARENT
     height = LayoutParams.MATCH_PARENT
 }
 
-public fun LayoutParams.height(size: Int): LayoutParams = apply {
+public fun <T : LayoutParams> T.height(size: Int): T = apply {
     height = size
 }
 
-public fun LayoutParams.width(size: Int): LayoutParams = apply {
+public fun <T : LayoutParams> T.width(size: Int): T = apply {
     width = size
 }
 
-public fun LayoutParams.wrapContentSize(): LayoutParams = apply {
+public fun <T : LayoutParams> T.wrapContentSize(): T = apply {
     width = LayoutParams.WRAP_CONTENT
     height = LayoutParams.WRAP_CONTENT
 }
 
-public fun LayoutParams.wrapContentWidth(): LayoutParams = apply {
+public fun <T : LayoutParams> T.wrapContentWidth(): T = apply {
     width = LayoutParams.WRAP_CONTENT
 }
 
-public fun LayoutParams.wrapContentHeight(): LayoutParams = apply {
+public fun <T : LayoutParams> T.wrapContentHeight(): T = apply {
     width = LayoutParams.WRAP_CONTENT
     height = LayoutParams.WRAP_CONTENT
 }
 
-public fun LayoutParams.maxWidth(): LayoutParams = apply {
+public fun <T : LayoutParams> T.maxWidth(): T = apply {
     width = LayoutParams.MATCH_PARENT
 }
 
-public fun LayoutParams.maxHeight(): LayoutParams = apply {
+public fun <T : LayoutParams> T.maxHeight(): T = apply {
     height = LayoutParams.MATCH_PARENT
+}
+
+public fun MarginLayoutParams.margin(size: Int): MarginLayoutParams = apply {
+    setMargins(size, size, size, size)
+}
+
+public fun MarginLayoutParams.marginStart(size: Int): MarginLayoutParams = apply {
+    marginStart = size
+}
+
+public fun MarginLayoutParams.marginEnd(size: Int): MarginLayoutParams = apply {
+    marginEnd = size
+}
+
+public fun MarginLayoutParams.marginBottom(size: Int): MarginLayoutParams = apply {
+    bottomMargin = size
+}
+
+public fun MarginLayoutParams.marginTop(size: Int): MarginLayoutParams = apply {
+    topMargin = size
 }
