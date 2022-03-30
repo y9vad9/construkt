6c0a4d2fadf78195b742018c6e6871feccc7a8e7
vadim <y9vad9@gmail.com>
2022-03-30 15:40:48 +0300

    docs&feat: added docs to annotations and scopes & new annotations

--- annotation/src/main/kotlin/com/construkt/annotation/ViewDSL.kt
+++ annotation/src/main/kotlin/com/construkt/annotation/ViewDSL.kt
@@ -1,4 +1,63 @@
 package com.construkt.annotation
 
+/**
+ * Code generation marker for views.
+ * Using annotated type there will be generated DSL from it.
+ * ## View
+ * ### Example
+ * ```kotlin
+ * @ViewDSL
+ * typealias TextView = android.widget.TextView
+ * ```
+ * ### Result
+ * ```kotlin
+ * interface TextViewDSLScope {
+ *  fun text(text: String)
+ *  fun textSize(textSize: Int)
+ *  ...
+ * }
+ *
+ * fun ViewGroup.textView(layoutParams: LayoutParams? = null, block: TextViewDSLScope.() -> Unit) {
+ *  addView(TextViewDSLScopeImpl().apply(block))
+ * }
+ *
+ * fun ViewGroup.textView(layoutParams: State<LayoutParams>, block: TextViewDSLScope.() -> Unit) {
+ *  addView(TextViewDSLScopeImpl().apply(block))
+ * }
+ *
+ * fun TextViewDSLScope.text(text: State<String>) {
+ *  lifecycleOwner.lifecycleScope.launch {
+ *      text.collect { text(it) }
+ *  }
+ * }
+ *
+ * fun TextViewDSLScope.textSize(textSize: State<Int>) {
+ *  lifecycleOwner.lifecycleScope.launch {
+ *      textSize.collect { textSize(it) }
+ *  }
+ * }
+ * ```
+ * ## LayoutParams
+ * Also, it generates extensions for layout params of views that annotated.
+ * ### Example
+ * ```kotlin
+ * public fun layoutParams(): ViewGroup.LayoutParams = ViewGroup.LayoutParams(-2, -2)
+ *
+ * public fun LinearLayout.LayoutParams.margins(
+ *  p0: Int,
+ *  p1: Int,
+ *  p2: Int,
+ *  p3: Int
+ * ): LinearLayout.LayoutParams {
+ *  setMargins(p0, p1, p2, p3)
+ *  return this
+ * }
+ * ```
+ * ## Builtins
+ * Also, there is some builtins for types that used in views:
+ * - [com.construkt.builtins.MenuDSLScope] for menus
+ *
+ * Consider contributing for builtins. We are always open for pull requests.
+ */
 @Target(AnnotationTarget.TYPEALIAS)
 annotation class ViewDSL
--- core/src/main/java/com/construkt/ViewScope.kt
+++ core/src/main/java/com/construkt/ViewScope.kt
@@ -8,6 +8,10 @@ import com.construkt.annotation.InternalConstruktApi
 
 /**
  * Basic scope of every DSL view builders.
+ * @property [context] - current view context.
+ * @property [origin] - view from which dsl was generated (it's used to access members that wasn't generated).
+ * @property [lifecycleOwner] - lifecycle owner of activity / fragment.
+ * @see [ViewGroupScope]
  */
 @ConstruktDSL
 public interface ViewScope<T : View> {
